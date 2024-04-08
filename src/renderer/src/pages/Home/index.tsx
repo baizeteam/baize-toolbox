e15c886b53ac61b650ec37b1c37a9b3a17e8ff11
xiaoyu <xiaoyu@sulg.top>
2024-04-08 22:24:51 +0800

    ✨ 调整主页

--- src/renderer/src/pages/Home/index.tsx
+++ src/renderer/src/pages/Home/index.tsx
@@ -1,19 +1,20 @@
 import React from "react";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { Button } from "antd";
+import "./index.module.less";
+import appIcon from "@renderer/assets/icon.ico";
 
 function App() {
-  const navigate = useNavigate();
-
-  const gotoSetting = () => {
-    navigate("/setting");
-  };
-
   return (
-    <div className="container">
-      <div>主页</div>
-      <Button onClick={gotoSetting}>goto setting</Button>
+    <div styleName="home">
+      <img
+        styleName="img"
+        onClick={() => window.open("https://github.com/sulgweb/baize-box")}
+        src={appIcon}
+      />
+
+      <div styleName="title">白泽工具箱</div>
+      <div styleName="desc">
+        白泽工具箱是一款功能强大的工具，可以帮助用户在不同的多媒体格式之间进行快速、高效的转换。无论是图片、音频还是视频，这个工具都能轻松搞定，让用户在处理多媒体文件时更加方便快捷。
+      </div>
     </div>
   );
 }
