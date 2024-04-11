d9110a1b42e05fb77b267299ace10353b258aab6
xiaoyu <xiaoyu@sulg.top>
2024-04-12 00:24:26 +0800

    ✨ 封装icon组件

---
+++ src/renderer/components/AppIcon/iconfont.js
@@ -0,0 +1,69 @@
+(window._iconfont_svg_string_4504422 =
+  '<svg><symbol id="baize-guanbi" viewBox="0 0 1024 1024"><path d="M208.4609375 128.9375l683.96484375 683.96484375c21.62109375 21.62109375 21.62109375 57.5859375 0 79.20703125-21.62109375 21.62109375-57.5859375 21.62109375-79.20703125 0L129.25390625 208.14453125c-14.44921875-21.62109375-14.44921875-57.5859375 0-79.20703125 21.62109375-21.62109375 57.5859375-21.62109375 79.20703125 0z m0 0" fill="" ></path><path d="M899.703125 208.14453125L215.6328125 892.109375c-21.62109375 21.62109375-57.5859375 21.62109375-79.20703125 0-21.62109375-21.62109375-21.62109375-57.5859375 0-79.20703125L820.49609375 128.9375c21.62109375-21.62109375 57.5859375-21.62109375 79.20703125 0 14.34375 21.62109375 14.34375 57.5859375 0 79.20703125z m0 0" fill="" ></path></symbol><symbol id="baize-zuidahua" viewBox="0 0 1024 1024"><path d="M880.91005631 90.125H332.55160842a52.70143661 52.70143661 0 0 0-52.70143661 52.70143661v105.40287324H143.08994369a52.70143661 52.70143661 0 0 0-52.70143661 52.70143661v579.71580276a52.70143661 52.70143661 0 0 0 52.70143661 52.70143662h579.71580277a52.70143661 52.70143661 0 0 0 52.70143662-52.70143662v-137.55074935h105.40287323a52.70143661 52.70143661 0 0 0 52.70143661-52.70143662V142.82643661a52.70143661 52.70143661 0 0 0-52.70143661-52.70143661z m-158.10430985 600.26936325V880.64654923H143.08994369V300.93074646h579.71580277z m155.99625238 0H775.50718308V300.93074646a52.70143661 52.70143661 0 0 0-52.70143662-52.70143661H332.55160842V142.82643661H880.91005631z" fill="" ></path></symbol><symbol id="baize-zuixiaohua" viewBox="0 0 1024 1024"><path d="M863.7 552.5H160.3c-10.6 0-19.2-8.6-19.2-19.2v-41.7c0-10.6 8.6-19.2 19.2-19.2h703.3c10.6 0 19.2 8.6 19.2 19.2v41.7c0 10.6-8.5 19.2-19.1 19.2z"  ></path></symbol><symbol id="baize-zuidahua1" viewBox="0 0 1024 1024"><path d="M770.9 923.3H253.1c-83.8 0-151.9-68.2-151.9-151.9V253.6c0-83.8 68.2-151.9 151.9-151.9h517.8c83.8 0 151.9 68.2 151.9 151.9v517.8c0 83.8-68.1 151.9-151.9 151.9zM253.1 181.7c-39.7 0-71.9 32.3-71.9 71.9v517.8c0 39.7 32.3 71.9 71.9 71.9h517.8c39.7 0 71.9-32.3 71.9-71.9V253.6c0-39.7-32.3-71.9-71.9-71.9H253.1z"  ></path></symbol></svg>'),
+  (function (n) {
+    var t = (t = document.getElementsByTagName("script"))[t.length - 1],
+      e = t.getAttribute("data-injectcss"),
+      t = t.getAttribute("data-disable-injectsvg");
+    if (!t) {
+      var i,
+        o,
+        a,
+        c,
+        d,
+        s = function (t, e) {
+          e.parentNode.insertBefore(t, e);
+        };
+      if (e && !n.__iconfont__svg__cssinject__) {
+        n.__iconfont__svg__cssinject__ = !0;
+        try {
+          document.write(
+            "<style>.svgfont {display: inline-block;width: 1em;height: 1em;fill: currentColor;vertical-align: -0.1em;font-size:16px;}</style>",
+          );
+        } catch (t) {
+          console && console.log(t);
+        }
+      }
+      (i = function () {
+        var t,
+          e = document.createElement("div");
+        (e.innerHTML = n._iconfont_svg_string_4504422),
+          (e = e.getElementsByTagName("svg")[0]) &&
+            (e.setAttribute("aria-hidden", "true"),
+            (e.style.position = "absolute"),
+            (e.style.width = 0),
+            (e.style.height = 0),
+            (e.style.overflow = "hidden"),
+            (e = e),
+            (t = document.body).firstChild
+              ? s(e, t.firstChild)
+              : t.appendChild(e));
+      }),
+        document.addEventListener
+          ? ~["complete", "loaded", "interactive"].indexOf(document.readyState)
+            ? setTimeout(i, 0)
+            : ((o = function () {
+                document.removeEventListener("DOMContentLoaded", o, !1), i();
+              }),
+              document.addEventListener("DOMContentLoaded", o, !1))
+          : document.attachEvent &&
+            ((a = i),
+            (c = n.document),
+            (d = !1),
+            r(),
+            (c.onreadystatechange = function () {
+              "complete" == c.readyState &&
+                ((c.onreadystatechange = null), l());
+            }));
+    }
+    function l() {
+      d || ((d = !0), a());
+    }
+    function r() {
+      try {
+        c.documentElement.doScroll("left");
+      } catch (t) {
+        return void setTimeout(r, 50);
+      }
+      l();
+    }
+  })(window);
---
+++ src/renderer/components/AppIcon/index.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import "./index.module.less";
+import "./iconfont.js";
+
+interface AppIconProps {
+  icon: string;
+  className?: string;
+  fontSize?: number | string;
+  color?: React.CSSProperties["color"];
+  [key: string]: any;
+}
+
+export default function AppIcon(props: AppIconProps) {
+  const { icon, className, fontSize, color, ...rest } = props;
+  const style: React.CSSProperties = {};
+  fontSize &&
+    (style.fontSize =
+      typeof fontSize === "number" ? `${fontSize}px` : fontSize);
+  color && (style.color = color);
+  const isSvg = icon.indexOf("#") === 0;
+
+  return isSvg ? (
+    <svg
+      className={`app-icon ${className || ""}`}
+      aria-hidden="true"
+      style={style}
+      {...rest}
+    >
+      <use xlinkHref={icon} />
+    </svg>
+  ) : (
+    <i
+      className={`app-icon iconfont ${icon} ${className || ""}`}
+      style={style}
+      {...rest}
+    />
+  );
+}
