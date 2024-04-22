137bd630be109fdd4a6d7995f58edc5933ce4ad3
xiaoyu <xiaoyu@sulg.top>
2024-04-22 23:23:33 +0800

    ✨ 注册截图快捷键

--- src/main/plugin/modules/keyboard.ts
+++ src/main/plugin/modules/keyboard.ts
@@ -1,18 +1,37 @@
-import { app, globalShortcut, BrowserWindow } from "electron";
+import {
+  app,
+  globalShortcut,
+  BrowserWindow,
+  desktopCapturer,
+  screen,
+} from "electron";
 
 app.whenReady().then(() => {
   escape();
+  registerScreenShot();
 });
 
 // 退出
 const escape = () => {
   globalShortcut.register("Escape", () => {
     console.log("Escape is pressed");
-    const allWin = BrowserWindow.getAllWindows();
-    allWin.forEach((win) => {
-      if (win["tag"] === "captureWin") {
-        win.hide();
-      }
+  });
+};
+
+const registerScreenShot = () => {
+  // 注册截图快捷键
+  globalShortcut.register("Alt+S", async () => {
+    console.log("Alt+S is pressed");
+    const mousePoint = screen.getCursorScreenPoint();
+    const display = screen.getDisplayNearestPoint(mousePoint);
+    console.log(display);
+    const sources = await desktopCapturer.getSources({
+      types: ["screen"],
+      thumbnailSize: display.size,
     });
+    const source = sources.find((source) => {
+      return source.display_id === String(display.id);
+    });
+    console.log(source);
   });
 };
