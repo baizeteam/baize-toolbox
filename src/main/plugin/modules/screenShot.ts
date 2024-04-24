14f9dd00652d7df6e0397ce6a944d213b25d4f6f
xiaoyu <xiaoyu@sulg.top>
2024-04-25 00:43:12 +0800

    ✨ 提取截图模块中的通用ipc

--- src/main/plugin/modules/screenShot.ts
+++ src/main/plugin/modules/screenShot.ts
@@ -18,6 +18,52 @@ const hideScreenShotWin = () => {
   }
 };
 
+// 显示截图窗口
+const showScreenShotWin = async () => {
+  if (screenShotWin) {
+    const mousePoint = screen.getCursorScreenPoint();
+    const display = screen.getDisplayNearestPoint(mousePoint);
+    const sources = await desktopCapturer.getSources({
+      types: ["screen"],
+      thumbnailSize: display.size,
+    });
+    const source = sources.find((source) => {
+      return source.display_id === String(display.id);
+    });
+    // screenShotWin.setBounds(display.bounds);
+    screenShotWin.setSize(display.size.width, display.size.height);
+    screenShotWin.setPosition(display.bounds.x, display.bounds.y);
+    enterWinFullScreen(screenShotWin);
+    screenShotWin.show();
+    screenShotWin.webContents.send("GET_SCREEN_SHOT_STREAM", {
+      source: source,
+      display: display,
+    });
+  }
+};
+
+// 创建图片窗口
+const createImageWin = async (data) => {
+  const { width, height, base64, x, y } = data;
+  const config = {
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    show: false,
+  };
+  if (x !== undefined && y !== undefined) {
+    config["x"] = x;
+    config["y"] = y;
+  }
+  const imageWin = await createWin({
+    config,
+    url: "/siteAssistTransprent/index.html#/image-win",
+    injectData: { base64 },
+  });
+  imageWin.setSize(width + 12, height + 12);
+  imageWin.show();
+};
+
 app.whenReady().then(async () => {
   screenShotWin = await createWin({
     config: {
@@ -38,24 +84,34 @@ app.whenReady().then(async () => {
     return true;
   });
 
+  // 显示截图窗口
+  ipcMain.handle("SCREEN_SHOT_WIN_SHOW", () => {
+    showScreenShotWin();
+    return true;
+  });
+
   // 创建图片窗口
   ipcMain.handle("SCREEN_SHOT_CREATE_IMAGE_WIN", async (event, data) => {
     hideScreenShotWin();
     const { display, cutInfo, base64 } = data;
-    const imageWin = await createWin({
-      config: {
-        frame: false,
-        transparent: true,
-        alwaysOnTop: true,
-        show: false,
-        x: display.bounds.x + cutInfo.startX,
-        y: display.bounds.y + cutInfo.startY,
-      },
-      url: "/siteAssistTransprent/index.html#/image-win",
-      injectData: { base64 },
+    createImageWin({
+      width: cutInfo.width,
+      height: cutInfo.height,
+      base64,
+      x: display.bounds.x + cutInfo.startX,
+      y: display.bounds.y + cutInfo.startY,
     });
-    imageWin.setSize(cutInfo.width + 12, cutInfo.height + 12);
-    imageWin.show();
+    return true;
+  });
+
+  // 打开图片窗口
+  ipcMain.handle("SCREEN_SHOT_OPEN_IMAGE_WIN", async (event, data) => {
+    const { cutInfo, base64 } = data;
+    createImageWin({
+      width: cutInfo.width,
+      height: cutInfo.height,
+      base64,
+    });
     return true;
   });
 
@@ -72,24 +128,7 @@ const escape = () => {
 
 const registerScreenShot = () => {
   // 注册截图快捷键
-  globalShortcut.register("Alt+S", async () => {
-    const mousePoint = screen.getCursorScreenPoint();
-    const display = screen.getDisplayNearestPoint(mousePoint);
-    const sources = await desktopCapturer.getSources({
-      types: ["screen"],
-      thumbnailSize: display.size,
-    });
-    const source = sources.find((source) => {
-      return source.display_id === String(display.id);
-    });
-    // screenShotWin.setBounds(display.bounds);
-    screenShotWin.setSize(display.size.width, display.size.height);
-    screenShotWin.setPosition(display.bounds.x, display.bounds.y);
-    enterWinFullScreen(screenShotWin);
-    screenShotWin.show();
-    screenShotWin.webContents.send("GET_SCREEN_SHOT_STREAM", {
-      source: source,
-      display: display,
-    });
+  globalShortcut.register("Alt+S", () => {
+    showScreenShotWin();
   });
 };
