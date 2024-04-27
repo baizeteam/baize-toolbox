a4806996596aa1f0120eb5640387c056de569d0e
xiaoyu <xiaoyu@sulg.top>
2024-04-28 00:09:01 +0800

    ✨  添加官网链接和github链接

--- src/renderer/siteMain/src/pages/Home/index.tsx
+++ src/renderer/siteMain/src/pages/Home/index.tsx
@@ -2,19 +2,38 @@ import React from "react";
 import "./index.module.less";
 import appIcon from "@renderer/assets/images/icon.ico";
 import { useTranslation } from "react-i18next";
+import { Button } from "antd";
 
 function App() {
   const { t } = useTranslation();
   return (
     <div styleName="home">
-      <img
-        styleName="img"
-        onClick={() => window.open("https://github.com/sulgweb/baize-toolbox")}
-        src={appIcon}
-      />
+      <div styleName="content">
+        <img
+          styleName="img"
+          onClick={() => window.open("https://baize.plume.vip")}
+          src={appIcon}
+        />
 
-      <div styleName="title">{t("siteMain.pages.home.title")}</div>
-      <div styleName="desc">{t("siteMain.pages.home.desc")}</div>
+        <div styleName="title">{t("siteMain.pages.home.title")}</div>
+        <div styleName="desc">{t("siteMain.pages.home.desc")}</div>
+      </div>
+      <div styleName="footer">
+        <Button
+          type="link"
+          onClick={() => window.open("https://baize.plume.vip")}
+        >
+          {t("siteMain.pages.home.officialWebsite")}
+        </Button>
+        <Button
+          type="link"
+          onClick={() =>
+            window.open("https://github.com/baizeteam/baize-toolbox")
+          }
+        >
+          Github
+        </Button>
+      </div>
     </div>
   );
 }
