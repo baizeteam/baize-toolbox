2785b84af7627a67624088018de8ee93758b93c3
饶宇鹏 <raoyupeng@raoyupengdeMacBook-Air.local>
2024-04-29 21:26:33 +0800

    ✨格式化文档

--- src/main/utils/autoLanuch.ts
+++ src/main/utils/autoLanuch.ts
@@ -2,30 +2,30 @@ import LinuxAutoLaunch from "easy-auto-launch";
 import { app } from "electron";
 
 export const autoLanuch = async (isAutoLaunchEnabled: boolean = true) => {
-	const electronIsDev = !app.isPackaged;
+  const electronIsDev = !app.isPackaged;
 
-	if (electronIsDev) {
-		return;
-	}
+  if (electronIsDev) {
+    return;
+  }
 
-	if (process.platform === "linux") {
-		const autoLauncher = new LinuxAutoLaunch({
-			name: app.getName(),
-			isHidden: false,
-			path: process.env.APPIMAGE
-		});
+  if (process.platform === "linux") {
+    const autoLauncher = new LinuxAutoLaunch({
+      name: app.getName(),
+      isHidden: false,
+      path: process.env.APPIMAGE,
+    });
 
-		if (isAutoLaunchEnabled) {
-			await autoLauncher.enable();
-		} else {
-			await autoLauncher.disable();
-		}
+    if (isAutoLaunchEnabled) {
+      await autoLauncher.enable();
+    } else {
+      await autoLauncher.disable();
+    }
 
-		return;
-	}
+    return;
+  }
 
-	app.setLoginItemSettings({
-		openAtLogin: isAutoLaunchEnabled,
-		openAsHidden: false,
-	});
-}
+  app.setLoginItemSettings({
+    openAtLogin: isAutoLaunchEnabled,
+    openAsHidden: false,
+  });
+};
