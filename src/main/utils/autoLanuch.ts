1f9c183c3eb7537b2ed81752e9f5bc0761a99dba
饶宇鹏 <raoyupeng@raoyupengdeMacBook-Air.local>
2024-04-29 20:21:34 +0800

    ✨新增开机自动启动功能

---
+++ src/main/utils/autoLanuch.ts
@@ -0,0 +1,31 @@
+import LinuxAutoLaunch from "easy-auto-launch";
+import { app } from "electron";
+
+export const autoLanuch = async (isAutoLaunchEnabled: boolean = true) => {
+	const electronIsDev = !app.isPackaged;
+
+	if (electronIsDev) {
+		return;
+	}
+
+	if (process.platform === "linux") {
+		const autoLauncher = new LinuxAutoLaunch({
+			name: app.getName(),
+			isHidden: false,
+			path: process.env.APPIMAGE
+		});
+
+		if (isAutoLaunchEnabled) {
+			await autoLauncher.enable();
+		} else {
+			await autoLauncher.disable();
+		}
+
+		return;
+	}
+
+	app.setLoginItemSettings({
+		openAtLogin: isAutoLaunchEnabled,
+		openAsHidden: false,
+	});
+}
